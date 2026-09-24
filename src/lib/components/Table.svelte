ffeb235f9704c42e5caad2735bc63bae7260539c
Jeremy Hage <jhage.codingkr@gmail.com>
2026-09-24 13:15:08 -0500

    feat(components): a table can carry a caption

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+  import type { Snippet } from 'svelte';
   import type { HTMLTableAttributes } from 'svelte/elements';
 
   type Props = HTMLTableAttributes & {
     striped?: boolean;
     compact?: boolean;
+    caption?: string | Snippet;
   };
 
-  let { striped = false, compact = false, class: className, children, ...rest }: Props = $props();
+  let {
+    striped = false,
+    compact = false,
+    caption,
+    class: className,
+    children,
+    ...rest
+  }: Props = $props();
 </script>
 
 <div class="table-wrapper">
@@ -14,6 +23,11 @@
     {...rest}
     class={['table', { 'table-striped': striped, 'table-compact': compact }, className]}
   >
+    {#if typeof caption === 'string'}
+      <caption>{caption}</caption>
+    {:else if caption !== undefined}
+      <caption>{@render caption()}</caption>
+    {/if}
     {@render children?.()}
   </table>
 </div>
