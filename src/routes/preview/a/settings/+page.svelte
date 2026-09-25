20943f332a9c0cff023a4a2cd7560dd590cebcae
Jeremy Hage <jhage.codingkr@gmail.com>
2026-09-24 20:44:00 -0500

    feat(settings): variant A settings shell at /preview/a/settings

---
+++ src/routes/preview/a/settings/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { useContainer } from '$lib/context';
+  import EngineAside from '$lib/domains/recognition/ui/engine/EngineAside.svelte';
+  import { EngineSettingsView } from '$lib/domains/recognition/ui/engine/engine-settings.svelte';
+  import EngineSettingsScreen from '$lib/domains/recognition/ui/engine/EngineSettingsScreen.svelte';
+  import SettingsShell from '../../../settings/redesign/a/SettingsShell.svelte';
+
+  const view = new EngineSettingsView(useContainer());
+
+  $effect(() => {
+    void view.load();
+    return () => view.dispose();
+  });
+</script>
+
+<SettingsShell current="engine" root="/preview/a/settings">
+  {#snippet aside()}
+    <EngineAside {view} />
+  {/snippet}
+  <div class="scheme-dark surface-bg min-h-screen">
+    <EngineSettingsScreen {view} />
+  </div>
+</SettingsShell>
